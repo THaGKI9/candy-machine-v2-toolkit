ea1afb0cf1aa95440ef6df288aed7fff54b1ceb6
Tony Ke <thagki9@outlook.com>
2022-05-24 12:54:45 +0800

    Add deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+function getAssetPrefix() {
+  if (process.env.GITHUB_ACTIONS === "true") {
+    const [, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    return `/${repo.toLowerCase()}/`;
+  }
+
+  return "";
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: getAssetPrefix(),
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
